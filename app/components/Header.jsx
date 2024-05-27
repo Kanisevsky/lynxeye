d8d3759564cecd81f9c7446d9484fe5c45b25e1b
Kanisevsky <meltsmokestudio@gmail.com>
2024-05-27 19:44:59 +0100

    header

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,13 +1,18 @@
 import Image from 'next/image';
 import React from 'react';
-import { logo } from '../constants';
+import { logo, navLinks } from '../constants';
+
 const Header = () => {
   return (
-    <header className="w-full">
-      <div className="">
-        <Image src={logo} alt="Lynxeye Logo" width={90} height={20} />
-      </div>
-      <p className="text-5xl">A trusted guide for your next step.</p>
+    <header className="w-full flex justify-between">
+      <Image src={logo} alt="Lynxeye Logo" width={90} height={20} />
+      <nav>
+        <ul className="flex space-x-5">
+          {navLinks.map((link) => (
+            <li key={link}>{link}</li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 };
---
+++ app/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <div>
+      <p className="text-5xl">A trusted guide for your next step.</p>
+    </div>
+  );
+};
+
+export default Hero;
